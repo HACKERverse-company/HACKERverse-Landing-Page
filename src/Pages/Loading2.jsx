fc3300ae871746442ce966a4c65d72f484d282df
Lilit Tamrazyan <tamrazyan.lilit.95@gmail.com>
2024-04-23 14:44:55 +0400

    Change loader

--- src/Pages/Loading2.jsx
+++ src/Pages/Loading2.jsx
@@ -1,22 +1,24 @@
 // Loader.js
-import React from 'react';
-import CustomLoaderSVG from '../img/support.svg'; // Import your custom SVG file
+import React from "react";
+import CustomLoaderSVG from "../img/Circle-Loading-ORB-gif.gif"; // Import your custom SVG file
 
-const Loader = ({text}) => {
-    return (
-        <div className="loader-overlay">
-            <div className="loader-container text-center">
-                <p className="text-2xl text-white mb-4 hover-underline"><i>{text}</i></p>
-                <div className="centered ml-24 ">
-                    <img src={CustomLoaderSVG} alt="Custom Loader" className="justify-center   animate-spin h-12 w-12" />
-                </div>
-            </div>
+const Loader = ({ text }) => {
+  return (
+    <div className="loader-overlay">
+      <div className="loader-container flex flex-col justify-center items-center text-center">
+        <p className="text-2xl text-white text-center mb-4 hover-underline">
+          <i>{text}</i>
+        </p>
+        <div className="centered ml-24 ">
+          <img
+            src={CustomLoaderSVG}
+            alt="Custom Loader"
+            className="justify-center   animate-spin h-24 w-24"
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Loader;
-
-
-
-
