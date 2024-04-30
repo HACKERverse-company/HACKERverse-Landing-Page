35c075dee3d681852e27159a66386b76c444ad69
Jibrand <muhammadjibrandev@gmail.com>
2024-04-30 11:10:50 -0700

    main: Added backend to the chatbot

--- src/Pages/Home/ChatBot.jsx
+++ src/Pages/Home/ChatBot.jsx
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import orbmp4 from "../../img/orb/orb.mp4";
 import BotLogo from '../../img/logogreen.png';
+import UserLogo from '../../img/user1.jpg';
+import toast, { Toaster } from 'react-hot-toast'
+import Confetti from 'react-confetti';
+import { Api } from '../../Api/Api';
 import Orb from '../../img/orb.gif';
 
 const ChatBot = () => {
-    //bot
+    const [messages, setMessages] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+    const [loading, setloading] = useState(false)
+    const messagesEndRef = useRef(null);
+    const inputRef = useRef(null);
     const [isOpen, setIsOpen] = useState(false);
     const [isOrbHover, setIsOrbHover] = useState(false)
 
@@ -23,6 +31,55 @@ const ChatBot = () => {
     const handleOrbHoverOut = () => {
         setIsOrbHover(false);
     };
+
+    const sendMessage = async (message) => {
+        const response = await fetch(`${Api}/query`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ query: message }),
+        });
+
+        const data = await response.json();
+        return data;
+    };
+
+    const handleSubmit = async (event) => {
+        if (inputValue == '' || inputValue == null) { toast.error('Please Enter your Message.'); return }
+
+        setloading(true)
+        event.preventDefault();
+        setMessages([
+            ...messages,
+            { type: 'user', text: inputValue },
+            { type: 'bot', text: '...' },
+        ]);
+        setInputValue(''); // Clear the input field after submission
+
+        const botResponse = await sendMessage(inputValue);
+
+        setMessages([
+            ...messages,
+            { type: 'user', text: inputValue },
+            { type: 'bot', text: botResponse },
+        ]);
+        setloading(false)
+    };
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [messages]);
+
+
     return (
         <>
             <div className="fixed -bottom-3 -right-3">
@@ -41,19 +98,49 @@ const ChatBot = () => {
                         <div className="">
                             <div className="flex-1 p:2 sm:p-2 justify-between flex flex-col h-[400px] bg-black rounded-2xl">
                                 <div id="messages" className="flex flex-col -mt-1 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch h-full bg-[#323232] rounded-md     custom-scrollbar">
-                                    <div className="chat-message">
-                                        <div className="flex items-end">
-                                            <div className="flex flex-col space-y-2 text-md max-w-xs mx-2 order-2 items-start">
-                                                <div> <span className="px-4 py-2 rounded-lg inline-block  rounded-bl-none bg-gray-300 text-gray-900  font-[19px]">  Welcome to the HACKERverse® How may I help You? </span> </div> </div>
-                                            <img src={BotLogo} alt="My profile" className="w-12 h-12 rounded-full order-1" />
+                                    <div  className={`chat-message mb-3 `}>
+                                        <div  className={`  flex items-end`}>
+                                            <div className={`flex flex-col space-y-2 text-md max-w-xs mx-2 order-2 items-start`}>
+                                                <div>
+                                                    <span className={`px-4 py-2 rounded-lg inline-block  rounded-bl-none bg-gray-300 text-gray-900  `}>
+                                                        Hey WhatsApp! How may I help you.
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <img src= {BotLogo} alt="Profile" className={`w-12 h-12 rounded-full order-1 `} />
                                         </div>
                                     </div>
+
+                                    {messages.map((message, index) => (
+                                        <div key={index} className={`chat-message mb-3 `}>
+                                            <div key={index} className={`  ${message.type === 'user' ? 'flex items-end justify-end' : 'flex items-end'}`}>
+                                                <div className={`flex flex-col space-y-2 text-md max-w-xs mx-2 order-${message.type === 'user' ? '1' : '2'} items-${message.type === 'user' ? 'end' : 'start'}`}>
+                                                    <div>
+                                                        <span className={`px-4 py-2 rounded-lg inline-block ${message.type === 'user' ? 'rounded-br-none bg-[#54721e] text-white ' : 'rounded-bl-none bg-gray-300 text-gray-900  '}`}>
+                                                            {message.text}
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                                <img src={message.type === 'user' ? UserLogo : BotLogo} alt="Profile" className={`w-12 h-12 rounded-full order-1 `} />
+                                            </div>
+                                        </div>
+                                    ))}
+                                    <div ref={messagesEndRef} />
                                 </div>
                                 <div className="  border-gray-200 px-1 pr-3 pt-2 mb-2 sm:mb-0 pb-2">
                                     <div className="relative flex">
-                                        <input type="text" placeholder="Write your message!" className="ml-1 w-full focus:outline-none focus:placeholder-gray-400 font-medium text-gray-900 placeholder-gray-600 pl-3 bg-gray-200 rounded-md py-2" />
+                                        <input type="text" placeholder="Write your message!"
+                                            ref={inputRef}
+                                            value={inputValue}
+                                            onChange={handleChange}
+                                            onKeyDown={(e) => {
+                                                if (e.key === 'Enter' && loading == false) {
+                                                    handleSubmit(e);
+                                                }
+                                            }}
+                                            className="ml-1 w-full focus:outline-none focus:placeholder-gray-400 font-medium text-gray-900 placeholder-gray-600 pl-3 bg-gray-200 rounded-md py-2" />
                                         <div className="absolute -right-1 items-center inset-y-0 hidden sm:flex">
-                                            <button type="button" className="inline-flex items-center justify-center rounded-r-md  -md px-2 py-2 transition duration-500 ease-in-out text-white bg-[#1f7920] hover:bg-green-900 focus:outline-none" > <span className="font-bold"></span> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 ml-2 transform rotate-90" > <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" /> </svg> </button>
+                                            <button disabled={loading} type="button" onClick={handleSubmit} className="inline-flex items-center justify-center rounded-r-md  -md px-2 py-2 transition duration-500 ease-in-out text-white bg-[#1f7920] hover:bg-green-900 focus:outline-none" > <span className="font-bold"></span> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 ml-2 transform rotate-90" > <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" /> </svg> </button>
                                         </div>
                                     </div>
                                 </div>
