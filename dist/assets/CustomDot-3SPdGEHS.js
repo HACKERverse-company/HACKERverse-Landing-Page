af57337dd4ed3ed8bbded15cb36a3617063a23de
Jibrand <muhammadjibrandev@gmail.com>
2024-05-16 06:37:59 -0700

    main: Fixed build

--- dist/assets/CustomDot-3SPdGEHS.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-B1V8ig0m.js";const n=({onClick:t,index:e,active:o})=>s.jsx("button",{className:`dot ${o?"active":"inactive"}`,onClick:()=>t()});export{n as C};
---
+++ dist/assets/CustomDot-DMwpleBT.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-CO5zVIbV.js";const n=({onClick:t,index:e,active:o})=>s.jsx("button",{className:`dot ${o?"active":"inactive"}`,onClick:()=>t()});export{n as C};
